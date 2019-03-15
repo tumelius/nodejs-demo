6b6acd13793e25944204f45e60e114d3a9fe8430
Tuomas <tuomas.puro@esedu.fi>
2019-03-15 10:47:24 +0000

    Lisättiin sivut

--- app.js
+++ app.js
@@ -3,3 +3,23 @@
 
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
+
+http.createServer(function (req, res){
+    var q = url.parse(req.url, true);
+    var filename = "./pages" + q.pathname;
+
+    fs.readFile(filename, function(err, data){
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        } 
+        
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+
+    });
+    
+}).listen(8080);
+
